1215b931b4a61dcf5c7922c1fe98dec40a8af054
RicBM71 <rbartolome@sanaval.com>
2022-01-05 16:18:57 +0100

    quito rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Cita;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -13,41 +12,37 @@ use Illuminate\Support\Facades\Route;
 // })->name('home');
 Route::middleware(['auth:sanctum', 'verified', 'lortad'])->get('/', [App\Http\Controllers\DashboardController::class, 'index'])->name('home');
 
-Route::get('/test', function () {
+// Route::get('/test', function () {
 
-    return Cita::with(['estado'])
-        ->paciente(806)
-        ->orderBy('fecha', 'desc')
-        ->get()->take(5);
+//     return Cita::with(['estado'])
+//         ->paciente(806)
+//         ->orderBy('fecha', 'desc')
+//         ->get()->take(5);
 
-})->name('test');
+// })->name('test');
 
 Route::middleware(['auth:sanctum', 'verified', 'lortad'])->get('dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 Route::middleware(['auth:sanctum', 'verified'])->get('lortad', [App\Http\Controllers\LortadController::class, 'index'])->name('lortad');
 Route::middleware(['auth:sanctum', 'verified'])->put('lortad', [App\Http\Controllers\LortadController::class, 'update'])->name('lortad.update');
 
-Route::group([
-    'prefix'     => 'admin',
-    // 'namespace' => 'Mto',
-    //'middleware' => ['auth:sanctum','verified','password.confirm']],
-    'middleware' => ['auth:sanctum', 'verified', 'lortad', 'role:root']],
-    function () {
+// Route::group([
+//     'prefix'     => 'admin',
+//     'middleware' => ['auth:sanctum', 'verified', 'lortad', 'role:root']],
+//     function () {
 
-        //Route::middleware('password.confirm')->resource('users', App\Http\Controllers\Admin\UsersController::class);
-        Route::resource('users', App\Http\Controllers\Admin\UsersController::class);
-        Route::delete('users/{user}/photo/delete', [App\Http\Controllers\Admin\UsersPhotoController::class, 'destroy'])->name('photo.destroy');
+//         Route::resource('users', App\Http\Controllers\Admin\UsersController::class);
+//         Route::delete('users/{user}/photo/delete', [App\Http\Controllers\Admin\UsersPhotoController::class, 'destroy'])->name('photo.destroy');
 
-        Route::get('users/{user}/roles', [App\Http\Controllers\Admin\UsersRolesController::class, 'show'])->name('user.roles.show');
-        Route::put('users/{user}/roles', [App\Http\Controllers\Admin\UsersRolesController::class, 'update'])->name('user.roles.update');
-        Route::get('users/{user}/permission', [App\Http\Controllers\Admin\UsersPermissionsController::class, 'show'])->name('user.permissions.show');
-        Route::put('users/{user}/permission', [App\Http\Controllers\Admin\UsersPermissionsController::class, 'update'])->name('user.permissions.update');
+//         Route::get('users/{user}/roles', [App\Http\Controllers\Admin\UsersRolesController::class, 'show'])->name('user.roles.show');
+//         Route::put('users/{user}/roles', [App\Http\Controllers\Admin\UsersRolesController::class, 'update'])->name('user.roles.update');
+//         Route::get('users/{user}/permission', [App\Http\Controllers\Admin\UsersPermissionsController::class, 'show'])->name('user.permissions.show');
+//         Route::put('users/{user}/permission', [App\Http\Controllers\Admin\UsersPermissionsController::class, 'update'])->name('user.permissions.update');
 
-        Route::resource('roles', App\Http\Controllers\Admin\RolesController::class);
+//         Route::resource('roles', App\Http\Controllers\Admin\RolesController::class);
 
-    }
-);
+//     }
+// );
 
 Route::middleware(['auth:sanctum', 'verified', 'lortad'])->get('book/{fecha}/{turno}', [App\Http\Controllers\ReservasController::class, 'index'])->name('book');
 Route::middleware(['auth:sanctum', 'verified', 'lortad'])->post('book/create', [App\Http\Controllers\ReservasController::class, 'store'])->name('book.store');
 Route::middleware(['auth:sanctum', 'verified', 'lortad'])->delete('book/{cita}', [App\Http\Controllers\ReservasController::class, 'destroy'])->name('book.destroy');
-//Route::resource('book', App\Http\Controllers\Admin\ReservasController::class);
